d483a5153c34ad54f7eccf308228fea05be0e574
Jinwook-Yang <100738755+Jinwook-Yang@users.noreply.github.com>
2024-06-04 14:10:22 +0900

    feat: add user info

--- src/modules/userInfo/index.ts
+++ src/modules/userInfo/index.ts
@@ -1,14 +1,20 @@
+import { UsersModel } from 'models/users';
 import knex from 'postgres/connection'
 
 const getUserInfo = async (userId: number) => {
+  const userInfo = await UsersModel.findById(userId);
+  console.log('User Email: ', userInfo.user_email);
+  console.log('User Name: ', userInfo.user_name);
   const result = await knex.raw(`
     SELECT * FROM users
     JOIN ratings ON users.id = ratings.user_id
     JOIN movies ON ratings.movie_id = movies.id
     WHERE users.id = ${userId};
   `);
-  console.log('User Email: ', result.rows[0].user_email);
-  console.log('User Name: ', result.rows[0].user_name);
+  if (result.rows.length === 0) {
+    console.log('No rated movies');
+    return;
+  }
   console.log('### Rated Movies ###');
   for (let i = 0; i < result.rows.length; i++) {
     console.log(`Title: ${result.rows[i].title}, Rating: ${result.rows[i].rating}`);
