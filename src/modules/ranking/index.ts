8df6080940759f0f3d34093d89907a1583abc56d
Jinwook-Yang <100738755+Jinwook-Yang@users.noreply.github.com>
2024-06-04 14:45:06 +0900

    feat: add ranking as query

--- src/modules/ranking/index.ts
+++ src/modules/ranking/index.ts
@@ -2,12 +2,13 @@ import knex from 'postgres/connection';
 
 const getMovieRanking = async () => {
   const result = await knex.raw(`
-    SELECT * FROM view_movie_ratings
-    ORDER BY avg_rating DESC LIMIT 10;
+    SELECT row_number() OVER (ORDER BY avg_rating DESC) AS rank, title, avg_rating
+    FROM view_movie_ratings
+    LIMIT 10;
   `);
   console.log('### Top 10 Movies ###');
   for (let i = 0; i < result.rows.length; i++) {
-    console.log(`${i + 1}. ${result.rows[i].title} (${result.rows[i].avg_rating})`);
+    console.log(`${result.rows[i].rank}. ${result.rows[i].title} (${result.rows[i].avg_rating})`);
   }
   console.log('### End ###');
 }
